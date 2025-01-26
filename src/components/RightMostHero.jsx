06dae58edf77edb985f897a66d8f0165abd99ff7
Miss-Ishika <101314286+Miss-Ishika@users.noreply.github.com>
2025-01-26 17:15:56 +0530

    change in hero section

--- src/components/RightMostHero.jsx
+++ src/components/RightMostHero.jsx
@@ -4,10 +4,10 @@ const RightMostHero = () => {
   return (
     <div className='relative'>
         <ul className='flex flex-col gap-12 mt-16'>
-          <li className='border-2 md:border-r-0 text-lg py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Annual Compliance</li>
-          <li className='border-2 md:border-r-0 text-lg py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Payroll Services</li>
-          <li className='border-2 md:border-r-0 text-lg py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Comapny Formation</li>
-          <li className='border-2 md:border-r-0 text-lg py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Annual Compliance</li>
+          <li className='border-2 md:border-r-0 text-md py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Annual Compliance</li>
+          <li className='border-2 md:border-r-0 text-md py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Payroll Services</li>
+          <li className='border-2 md:border-r-0 text-md py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Comapny Formation</li>
+          <li className='border-2 md:border-r-0 text-md py-4 mx-8 md:mx-0 md:py-2 md:px-[10px] font-semibold md:rounded-l-xl shadow-xl flex justify-center items-center'>Annual Compliance</li>
         </ul>
     </div>
   )
